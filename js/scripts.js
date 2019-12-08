8aacf7008b55b8bf3c6e1f638d83ca98db1678de
Louis-crypto <kinglouis745@gmail.com>
2019-12-08 16:12:25 +0300

    style icons paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 $(document).ready(function () {
   $("#designIcon").click(function () {
-    $(this).hide();
-  });
-  $("#designIcon").click(function(){
     $("#design").show();
   });
+
+
   $("#devIcon").click(function () {
     $(this).hide();
   });
   $("#devIcon").click(function(){
     $("#development").show();
+  });
+  $("#prodIcon").click(function () {
+    $(this).hide();
+  });
+  $("#prodIcon").click(function(){
+    $("#product").show();
   });
 });
