ef7b87f8ae99ef4a5826b5cbf58ec1b11e39c0ca
Louis-crypto <kinglouis745@gmail.com>
2019-12-09 14:45:48 +0300

    add padding to the images

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
 $(document).ready(function () {
-  $("#designIcon").click(function () {
-    $("#design").show();
-      $("#designIcon").hide();
-      $("#design").on("click",function(){
-        $("#design").hide();
-        $("designIcon").show();
-      });
-    
+  var userName = document.getElementById("name").value;
+  $("#submit").on("click", function(){
+    alert("dear" + " " + userName + " " + "we have received your message");
+  });
+   $("#designIcon").click(function () {
+     
   });
 
 
-  $("#devIcon").click(function () {
-    $(this).hide();
-  });
-  $("#devIcon").click(function(){
-    $("#development").show();
-  });
-  $("#prodIcon").click(function () {
-    $(this).hide();
-  });
-  $("#prodIcon").click(function(){
-    $("#product").show();
-  });
+  // $("#devIcon").click(function () {
+  //   $(this).hide();
+  // });
+  // $("#devIcon").click(function(){
+  //   $("#development").show();
+  // });
+  // $("#prodIcon").click(function () {
+  //   $(this).hide();
+  // });
+  // $("#prodIcon").click(function(){
+  //   $("#product").show();
+  // });
+
 });
