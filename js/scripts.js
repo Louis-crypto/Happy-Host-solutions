c0c3128fa8e72b38fdf9b9ba1f4745ddcd714817
Louis-crypto <kinglouis745@gmail.com>
2019-12-09 15:28:28 +0300

    add jquery togle effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,33 @@
 $(document).ready(function () {
   var userName = document.getElementById("name").value;
-  $("#submit").on("click", function(){
+  $("#submit").on("click", function () {
     alert("dear" + " " + userName + " " + "we have received your message");
   });
-   $("#designIcon").click(function () {
-     
+
+  $("#designIcon").on("click", function () {
+    $("#design").show();
+    $("#designIcon").hide();
+  });
+  $("#design").on("click", function () {
+    $("#designIcon").show();
+    $("#design").hide();
   });
 
+  $("#devIcon").on("click", function () {
+    $("#development").show();
+    $("#devIcon").hide();
+  });
+  $("#development").on("click", function () {
+    $("#devIcon").show();
+    $("#development").hide();
+  });
 
-  // $("#devIcon").click(function () {
-  //   $(this).hide();
-  // });
-  // $("#devIcon").click(function(){
-  //   $("#development").show();
-  // });
-  // $("#prodIcon").click(function () {
-  //   $(this).hide();
-  // });
-  // $("#prodIcon").click(function(){
-  //   $("#product").show();
-  // });
-
+  $("#prodIcon").on("click", function () {
+    $("#product").show();
+    $("#prodIcon").hide();
+  });
+  $("#product").on("click", function () {
+    $("#prodIcon").show();
+    $("#product").hide();
+  });
 });
