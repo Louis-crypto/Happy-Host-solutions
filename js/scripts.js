29cab77daa64ddae410af3e8838fbeb622b8794f
Louis-crypto <kinglouis745@gmail.com>
2019-12-09 22:22:47 +0300

    add hover to the porfolios

--- js/scripts.js
+++ js/scripts.js
@@ -37,6 +37,48 @@ $(document).ready(function () {
     $("#caption").fadeOut(500);
   });
 
+  $("#work2").mouseover(function(){
+    $("#caption1").fadeIn(500);
+  }).mouseout(function(){
+    $("#caption1").fadeOut(500);
+  });
+
+  $("#work3").mouseover(function(){
+    $("#caption2").fadeIn(500);
+  }).mouseout(function(){
+    $("#caption2").fadeOut(500);
+  });
+
+  $("#work4").mouseover(function(){
+    $("#caption3").fadeIn(500);
+  }).mouseout(function(){
+    $("#caption3").fadeOut(500);
+  });
+
+  $("#work5").mouseover(function(){
+    $("#caption4").fadeIn(500);
+  }).mouseout(function(){
+    $("#caption4").fadeOut(500);
+  });
+
+  $("#work6").mouseover(function(){
+    $("#caption5").fadeIn(500);
+  }).mouseout(function(){
+    $("#caption5").fadeOut(500);
+  });
+
+  $("#work7").mouseover(function(){
+    $("#caption6").fadeIn(500);
+  }).mouseout(function(){
+    $("#caption6").fadeOut(500);
+  });
+
+  $("#work8").mouseover(function(){
+    $("#caption7").fadeIn(500);
+  }).mouseout(function(){
+    $("#caption7").fadeOut(500);
+  });
+
   // $("#work1").hover(function(){
   //   $(this).css({opacity: 0.3});
   // }); 
